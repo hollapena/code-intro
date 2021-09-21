88c077b5fc659021930d93c8a58c0dc0f012aeb7
Callen Schultz <callen.schultz@gmail.com>
2021-09-20 23:05:00 -0500

    Adding extra features to calculator

--- calculator.js
+++ calculator.js
@@ -13,25 +13,39 @@ reader.question("What would you like to calculate?", function(input){
     mathSymbol = tokens[0];
     num1 = Number(tokens[1]);
     num2 = Number(tokens[2]);
+    num3 = Number(tokens[3]);
 
     console.log('mathSymbol', mathSymbol);
     console.log('num1', num1);
     console.log('num2', num2);
+    console.log('num3', num3);
 
 if (mathSymbol === "+"){
-    console.log(num1+num2);
+    console.log(num1+num2+num3);
 }
 if(mathSymbol === "-"){
-    console.log(num1-num2);
+    console.log(num1-num2-num3);
 }
 if(mathSymbol === "/"){
-    console.log(num1/num2);
+    console.log(num1/num2/num3);
 }
 if(mathSymbol === "*"){
-    console.log(num1*num2);
+    console.log(num1*num2*num3);
 }
 if(mathSymbol === "sqrt"){
     console.log(Math.sqrt(num1));
+}
+if(mathSymbol === "square"){
+    console.log(num1*num1);
+}
+if(mathSymbol === "cube"){
+    console.log(num1*num1*num1);
+}
+if(mathSymbol === "power"){
+    console.log(Math.pow(num1, num2));
+}
+if(mathSymbol === "remainder"){
+    console.log(num1%num2);
 }
     // This line closes the connection to the command line interface.
     reader.close()    
